e8b8bf77e7cf0fb6a48da14755ae90ad287033aa
lamb <1838940294@qq.com>
2023-08-30 09:43:54 +0800

    feat: 完成第12章课后练习

---
+++ chapter12/12_6.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+std::vector<int> *create_vi()
+{
+  return new std::vector<int>;
+}
+
+void push_vi(std::vector<int> *p)
+{
+  int i;
+  while (std::cin >> i)
+    p->push_back(i);
+}
+
+void print_vi(std::vector<int> *p)
+{
+  for (const auto i : (*p))
+    std::cout << i << std::endl;
+}
+
+int main()
+{
+  auto p = create_vi();
+  push_vi(p);
+  print_vi(p);
+  delete p;
+
+  return 0;
+}
